69bf69a2f1a263f3b32d1ea621df18243361cd3f
gemeln <mtang317@gmail.com>
2021-10-02 12:11:34 -0500

    Added prefab folder

---
+++ Assets/Scripts/TriggerGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerGenerator : MonoBehaviour
+{
+    public GameObject generator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void doGeneration()
+    {
+        Instantiate(generator, gameObject.transform.position +  new Vector3(480/32,0,0),Quaternion.identity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("NEW GENER");
+            doGeneration();
+            Destroy(gameObject);
+        }
+        
+    }
+}
