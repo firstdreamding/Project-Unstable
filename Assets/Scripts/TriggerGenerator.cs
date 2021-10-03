597a5d7a133df4f60091079aa55a81e8bf5ef497
firstdreamding <huaxu3721@yahoo.com>
2021-10-03 18:26:39 -0500

    Level Generation Done

--- Assets/Scripts/TriggerGenerator.cs
+++ Assets/Scripts/TriggerGenerator.cs
@@ -11,12 +11,15 @@ public class TriggerGenerator : MonoBehaviour
 
 
     public static bool initialized = false;
+    private static Vector3 lastPlatform;
     // Start is called before the first frame update
     void Start()
     {
         if (!initialized)
         {
             initialized = true;
+            lastPlatform = new Vector3(GameObject.Find("Player").transform.position.x, 0, 0);
+            Instantiate(AetherPlatform, lastPlatform, Quaternion.identity);
             doGeneration(new Vector3(-screenWidth, 0, 0),false);
         }
     }
@@ -29,15 +32,21 @@ public class TriggerGenerator : MonoBehaviour
 
     private void doGeneration(Vector3 pos, bool doNextGenerator = true)
     {
-        Vector3 newLocation =pos + new Vector3(screenWidth, 0, 0);
+        Vector3 newLocation = pos + new Vector3(screenWidth, 0, 0);
         if (doNextGenerator)
         {
             Instantiate(generator, newLocation, Quaternion.identity);
         }
-        for (int i = 0; i < 10;++i)
+        int i = 0;
+        while (i < 20 && lastPlatform.x < newLocation.x + 2 * screenWidth)
         {
-            Vector3 platformLoc = newLocation + new Vector3(screenWidth + screenWidth * Random.Range(-0.5f, 0.5f), screenHeight * Random.Range(-0.5f, 0.5f), 0);
-            Instantiate(AetherPlatform, platformLoc, Quaternion.identity);
+            i++;
+            float new_x = Random.Range(4f, 10f);
+            float new_y_ceil = Mathf.Min((-0.12f * Mathf.Pow(new_x - 5, 2) + 3) + lastPlatform.y, screenHeight / 2f - 1);
+            float new_y = Random.Range(screenHeight / -2f + 1, new_y_ceil);
+            lastPlatform = new Vector3(new_x + lastPlatform.x, new_y, 0);
+            Instantiate(AetherPlatform, lastPlatform, Quaternion.identity);
+            Debug.Log("This is initing " + lastPlatform + " and highest can jump is " + ((-0.12f * Mathf.Pow(new_x - 5, 2) + 3) + lastPlatform.y));
         }
     }
 
